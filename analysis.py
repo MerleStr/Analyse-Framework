99203092a4bafcff50b2da5bee8e37b9dbfe409e
MerleStr <merle-struckmann5.8@t-online.de>
2020-07-25 04:25:36 +0200

    adding psnr

--- analysis.py
+++ analysis.py
@@ -18,36 +18,54 @@ if not sys.warnoptions:
 
 tf.enable_eager_execution()
 
+scene = "Scene_1"
+
 #Example: https://www.tensorflow.org/api_docs/python/tf/image/ssim
-img_path1 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/original.jpg')
+img_path1 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/original.png')
 img1 = tf.image.decode_jpeg(img_path1, channels=3)
-img_path2 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/Upscaled/vmf_upscaled.png')
+img_path2 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/Upscaled/vmf_upscaled.png')
 img2 = tf.image.decode_png(img_path2, channels=3)
-img_path3 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/Upscaled/bicubic_upscaled.png')
+img_path3 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/Upscaled/avmf_upscaled.png')
 img3 = tf.image.decode_png(img_path3, channels=3)
-img_path4 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/Upscaled/content-adaptive_upscaled.png')
+img_path4 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/Upscaled/cmf_upscaled.png')
 img4 = tf.image.decode_png(img_path4, channels=3)
-img_path5 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/Upscaled/perceptual_upscaled.png')
+img_path5 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/Upscaled/bicubic_upscaled.png')
 img5 = tf.image.decode_png(img_path5, channels=3)
-img_path6 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/Upscaled/avmf_upscaled.png')
+img_path6 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/Upscaled/content-adaptive_upscaled.png')
 img6 = tf.image.decode_png(img_path6, channels=3)
-img_path7 = tf.io.read_file('C:/Users/merle/Pictures/img/Scene_10/Upscaled/cmf_upscaled.png')
+img_path7 = tf.io.read_file('C:/Users/merle/Pictures/img/' + scene + '/Upscaled/perceptual_upscaled.png')
 img7 = tf.image.decode_png(img_path7, channels=3)
 
-names = ['Vector Median Filter','Bicubic','Content-Adaptive','Perceptual','Anti Vector Median Filter','Color Median Filter']
+
+
+names = ['Vector Median Filter','Anti Vector Median Filter','Color Median Filter','Bicubic','Content-Adaptive','Perceptual']
 
 w = [img2.shape[1]]
 h = [img2.shape[0]]
 
-ssim_result1 = tf.image.ssim(img1, img2, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
-ssim_result2 = tf.image.ssim(img1, img3, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
-ssim_result3 = tf.image.ssim(img1, img4, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
-ssim_result4 = tf.image.ssim(img1, img5, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
-ssim_result5 = tf.image.ssim(img1, img6, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
-ssim_result6 = tf.image.ssim(img1, img7, max_val=255, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+im1 = tf.image.convert_image_dtype(img1, tf.float32)
+im2 = tf.image.convert_image_dtype(img2, tf.float32)
+im3 = tf.image.convert_image_dtype(img3, tf.float32)
+im4 = tf.image.convert_image_dtype(img4, tf.float32)
+im5 = tf.image.convert_image_dtype(img5, tf.float32)
+im6 = tf.image.convert_image_dtype(img6, tf.float32)
+im7 = tf.image.convert_image_dtype(img7, tf.float32)
+
+psnr_result1 = tf.image.psnr(im1, im2, max_val=1.0)
+psnr_result2 = tf.image.psnr(im1, im3, max_val=1.0)
+psnr_result3 = tf.image.psnr(im1, im4, max_val=1.0)
+psnr_result4 = tf.image.psnr(im1, im5, max_val=1.0)
+psnr_result5 = tf.image.psnr(im1, im6, max_val=1.0)
+psnr_result6 = tf.image.psnr(im1, im7, max_val=1.0)
+psnr = [psnr_result1.numpy(),psnr_result2.numpy(),psnr_result3.numpy(),psnr_result4.numpy(),psnr_result5.numpy(),psnr_result6.numpy()]
+
+ssim_result1 = tf.image.ssim(im1, im2, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+ssim_result2 = tf.image.ssim(im1, im3, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+ssim_result3 = tf.image.ssim(im1, im4, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+ssim_result4 = tf.image.ssim(im1, im5, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+ssim_result5 = tf.image.ssim(im1, im6, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
+ssim_result6 = tf.image.ssim(im1, im7, max_val=1.0, filter_size=11, filter_sigma=1.5, k1=0.01, k2=0.03)
 ssim = [ssim_result1.numpy(),ssim_result2.numpy(),ssim_result3.numpy(),ssim_result4.numpy(),ssim_result5.numpy(),ssim_result6.numpy()]
-
-
 
 image0 = tf.image.per_image_standardization(img1)
 image1 = tf.image.per_image_standardization(img2)
@@ -88,9 +106,9 @@ lp = [distance1, distance2, distance3, distance4, distance5, distance6]
 #Example PrettyTables: https://www.youtube.com/watch?v=gryi-dcF_mY
 #Dokumentation PrettyTables: https://ronisbr.github.io/PrettyTables.jl/v0.7/ 
 
-table = PrettyTable(['Name', 'SSIM', 'LPIPS'])
+table = PrettyTable(['Name', 'PSNR', 'SSIM', 'LPIPS'])
 for x in range(0,6):
-    table.add_row([names[x],ssim[x],lp[x]])
+    table.add_row([names[x],psnr[x],ssim[x],lp[x]])
 print(table)
 
 #print(tabulate([["Name","Age"],["Alice",24],["Bob",19]],headers="firstrow")
